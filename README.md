e079200d32d16263c35930e095538ba1560b90d4
Leksmanny <olalereolamilekan567@gmail.com>
2022-06-24 23:36:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSintro
+A javascript code that prints out your name, height, and country on the screen
+Github Page Link https://leksmannydev.github.io/Javascript-intro/
---
+++ app.js
@@ -0,0 +1,13 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+let firstName = "Olalere", 
+    lastName = "Ridwan Olamilekan",
+    height = "6ft",
+    country = "Nigeria",
+
+    fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+alert(`${fullName} \n${height} \n${country}`)
